e8bd2920361e15d66e1ecfc55412602effa99ab7
Gwynerva <the.gwynerva@gmail.com>
2026-06-03 16:38:43 +0300

    Responsive asset modal aside panel

--- app/modals/asset-modal/AssetModal.vue
+++ app/modals/asset-modal/AssetModal.vue
@@ -4,12 +4,17 @@ import AsideModalButton from './AssetModalButton.vue';
 defineProps<{
   asideTitle?: string;
 }>();
+
+const isAsideOpen = ref(true);
 </script>
 
 <template>
   <section class="absolute flex h-dvh w-dvw flex-col sm:flex-row">
     <div class="relative flex flex-1 items-center justify-center bg-bg-1">
-      <div class="absolute top-0 right-0 z-10 flex flex-col gap-sm p-sm">
+      <div
+        class="absolute top-0 right-0 z-10 flex flex-row-reverse gap-sm p-sm
+          sm:flex-col"
+      >
         <AsideModalButton icon="close" @click="closeModal" />
         <slot name="buttons"></slot>
       </div>
@@ -17,17 +22,34 @@ defineProps<{
       <slot name="preview"></slot>
     </div>
     <div
-      class="relative w-full border-t-2 border-border-1 bg-bg-2
+      class="relative flex w-full flex-col border-t-2 border-border-1 bg-bg-2
         shadow-[0_0_10px_6px_var(--color-shadow-3)] sm:max-w-75 sm:border-t-0
         sm:border-l sm:shadow-[0_0_10px_2px_var(--color-shadow-3)]"
     >
       <div
-        class="flex justify-between border-b border-border-1 px-md py-sm
-          font-bold tracking-tight text-text-2"
+        class="flex shrink-0 items-center justify-between border-b
+          border-border-1 px-md py-sm font-bold tracking-tight text-text-2"
       >
         <div>{{ asideTitle || phrase.asset }}</div>
+        <button
+          class="flex cursor-pointer items-center justify-center text-text-2
+            transition sm:hidden hocus:text-text-1"
+          @click="isAsideOpen = !isAsideOpen"
+        >
+          <Icon
+            :name="isAsideOpen ? 'collapse-vertical' : 'expand-vertical'"
+            class="size-5"
+          />
+        </button>
       </div>
-      <slot name="aside"></slot>
+      <div
+        class="aside-content-panel"
+        :style="{ '--aside-h': isAsideOpen ? 'auto' : '0px' }"
+      >
+        <div class="max-h-[60dvh] overflow-y-auto sm:h-full sm:max-h-none">
+          <slot name="aside"></slot>
+        </div>
+      </div>
     </div>
   </section>
 </template>
--- app/modals/edit-file/ModalEditFile.vue
+++ app/modals/edit-file/ModalEditFile.vue
@@ -65,7 +65,7 @@ const mediaPreview =
       </AssetModalButton>
     </template>
     <template #aside>
-      <div>Formalism</div>
+      <div class="h-[1000px]">Formalism</div>
     </template>
   </AssetModal>
 </template>
